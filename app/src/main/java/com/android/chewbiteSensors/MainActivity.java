e7cd2755bc65b337e142fc01e0493d8f4152cab1
J-Manzo <joseam20@yahoo.com.ar>
2024-04-05 18:15:19 -0300

    First UI previews.
    It was about bringing the design made by the UX/UI team to the app screens.
    
    It is expected to be able to develop the screens as defined in the document.

--- app/src/main/java/com/android/chewbiteSensors/MainActivity.java
+++ app/src/main/java/com/android/chewbiteSensors/MainActivity.java
@@ -24,6 +24,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(binding.getRoot());
 
         BottomNavigationView navView = findViewById(R.id.nav_view);
+        //BottomNavigationView customBottomNavigationView = findViewById(R.id.customBottomNavigationView);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
