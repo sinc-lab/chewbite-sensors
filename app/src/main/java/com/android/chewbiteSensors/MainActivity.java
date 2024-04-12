97aa1c99ffa743361c62c6a783dd2c44bca2fd3e
J-Manzo <joseam20@yahoo.com.ar>
2024-04-12 17:50:59 -0300

    Screen without ActionBar and with new styles
    The top bar with the screen name was removed.
    Added styling for secondary buttons.
    
    It is expected that the initial screen will be as close to the design as possible.

--- app/src/main/java/com/android/chewbiteSensors/MainActivity.java
+++ app/src/main/java/com/android/chewbiteSensors/MainActivity.java
@@ -15,9 +15,12 @@ import com.android.chewbiteSensors.databinding.ActivityMainBinding;
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
+    private  String tag = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        //Prueba Log
+        android.util.Log.d(tag, "onCreate");
         super.onCreate(savedInstanceState);
 
         binding = ActivityMainBinding.inflate(getLayoutInflater());
@@ -32,8 +35,39 @@ public class MainActivity extends AppCompatActivity {
                 R.id.navigation_settings, R.id.navigation_predictions)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        //NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
+
+
     }
 
+    @Override
+    protected void onStart() {
+        android.util.Log.d(tag, "onStart");
+        super.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+        android.util.Log.d(tag, "onResume");
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        android.util.Log.d(tag, "onPause");
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        android.util.Log.d(tag, "onStop");
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        android.util.Log.d(tag, "onDestroy");
+        super.onDestroy();
+    }
 }
