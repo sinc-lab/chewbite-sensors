7260d988b7efa59bc5275ed6aecb899054b4f13f
J-Manzo <joseam20@yahoo.com.ar>
2024-10-07 22:58:47 -0300

    Save spinner state.
    Code has been added to save the state of the configuration dropdown lists.
    
    It is expected that when you exit the app you can do so and re-enter the app to restore the configuration preferences.

--- app/src/main/java/com/android/chewbiteSensors/ui/audio/AudioFragment.java
+++ app/src/main/java/com/android/chewbiteSensors/ui/audio/AudioFragment.java
@@ -7,7 +7,10 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.CompoundButton;
+import android.widget.Spinner;
 import android.widget.Switch;
 
 import androidx.annotation.NonNull;
@@ -22,6 +25,9 @@ public class AudioFragment extends Fragment {
     private final String tag = "MainActivity";
     private static final String PREFS_KEY = "status_controls";
     private static final String STATUS_SWT_SOUND_CONFIG = "status_switch_sound_configuration";
+    private static final String STATUS_SPN_BIT_RATE_CONFIG = "status_switch_bit_rate_configuration";
+    private static final String STATUS_SPN_FREQUENCY_CONFIG = "status_switch_frequency_sound_configuration";
+    private static final String STATUS_SPN_FILE_TYPE_CONFIG = "status_switch_file_type_configuration";
     private static boolean switchSoundState;
     @SuppressLint("UseSwitchCompatOrMaterialCode")
     Switch switchAudioSetting;
@@ -56,6 +62,83 @@ public class AudioFragment extends Fragment {
         });
         /*----------------------------------------------------------------------------------------*/
 
+        // Inicializa del Spinner
+        Spinner spinnerBitRateConfiguration = view.findViewById(R.id.spn_bit_rate);
+        Spinner spinnerFrequencyAudioConfiguration = view.findViewById(R.id.spn_frequency_options);
+        Spinner spinnerFileTypeConfiguration = view.findViewById(R.id.spn_file_type);
+
+        // Obtén el array de opciones desde los recursos
+        String[] optionsBitRateArray = getResources().getStringArray(R.array.text_bit_rate_options);
+        String[] optionsFrequencyArray = getResources().getStringArray(R.array.text_frequency_options);
+        String[] optionsFileTypeArray = getResources().getStringArray(R.array.text_type_file_options);
+
+        // Crea un ArrayAdapter usando el array de opciones y un diseño simple para el spinner
+        ArrayAdapter<String> adapterBitRate = new ArrayAdapter<>(requireActivity(), android.R.layout.simple_spinner_item, optionsBitRateArray);
+        ArrayAdapter<String> adapterFrequency = new ArrayAdapter<>(requireActivity(), android.R.layout.simple_spinner_item, optionsFrequencyArray);
+        ArrayAdapter<String> adapterFileType = new ArrayAdapter<>(requireActivity(), android.R.layout.simple_spinner_item, optionsFileTypeArray);
+
+        // Especifica el diseño a utilizar cuando se despliega el spinner
+        adapterBitRate.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        adapterFrequency.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        adapterFileType.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        // Aplica el adapter al spinner
+        spinnerBitRateConfiguration.setAdapter(adapterBitRate);
+        spinnerFrequencyAudioConfiguration.setAdapter(adapterFrequency);
+        spinnerFileTypeConfiguration.setAdapter(adapterFileType);
+
+        // Recuperar la última selección guardada en SharedPreferences
+        int selectedBitRatePosition = sharedPreferences.getInt(STATUS_SPN_BIT_RATE_CONFIG, 0); // 0 es el valor por defecto (primera opción)
+        int selectedFrequencyPosition = sharedPreferences.getInt(STATUS_SPN_FREQUENCY_CONFIG, 0); // 0 es el valor por defecto (primera opción)
+        int selectedFileTypePosition = sharedPreferences.getInt(STATUS_SPN_FILE_TYPE_CONFIG, 0); // 0 es el valor por defecto (primera opción)
+        // Seleccionar la opción guardada
+        spinnerBitRateConfiguration.setSelection(selectedBitRatePosition);
+        spinnerFrequencyAudioConfiguration.setSelection(selectedFrequencyPosition);
+        spinnerFileTypeConfiguration.setSelection(selectedFileTypePosition);
+
+        // Guardar la selección del Spinner
+        spinnerBitRateConfiguration.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                SharedPreferences.Editor editor = sharedPreferences.edit();
+                // Guardar la posición seleccionada en SharedPreferences
+                editor.putInt(STATUS_SPN_BIT_RATE_CONFIG, position);
+                editor.apply();
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                // No se necesita implementar, pero es necesario sobrescribir el método
+            }
+        });
+        // Guardar la selección del Spinner
+        spinnerFrequencyAudioConfiguration.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                SharedPreferences.Editor editor = sharedPreferences.edit();
+                // Guardar la posición seleccionada en SharedPreferences
+                editor.putInt(STATUS_SPN_FREQUENCY_CONFIG, position);
+                editor.apply();
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                // No se necesita implementar, pero es necesario sobrescribir el método
+            }
+        });
+        // Guardar la selección del Spinner
+        spinnerFileTypeConfiguration.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                SharedPreferences.Editor editor = sharedPreferences.edit();
+                // Guardar la posición seleccionada en SharedPreferences
+                editor.putInt(STATUS_SPN_FILE_TYPE_CONFIG, position);
+                editor.apply();
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                // No se necesita implementar, pero es necesario sobrescribir el método
+            }
+        });
+        /*----------------------------------------------------------------------------------------*/
         return view;
     }
 
--- app/src/main/java/com/android/chewbiteSensors/ui/gps/GPSFragment.java
+++ app/src/main/java/com/android/chewbiteSensors/ui/gps/GPSFragment.java
@@ -7,7 +7,10 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.CompoundButton;
+import android.widget.Spinner;
 import android.widget.Switch;
 
 import androidx.annotation.NonNull;
@@ -21,6 +24,7 @@ public class GPSFragment extends Fragment {
     private GPSViewModel mViewModel;
     private static final String PREFS_KEY = "status_controls";
     private static final String STATUS_SWT_GPS_CONFIG = "status_switch_gps_configuration";
+    private static final String STATUS_SPN_FREQUENCY_GPS_CONFIG = "status_switch_frequency_gps_configuration";
     private static boolean switchGpsState;
     @SuppressLint("UseSwitchCompatOrMaterialCode")
     Switch switchGpsSetting;
@@ -52,7 +56,43 @@ public class GPSFragment extends Fragment {
             }
         });
         /*----------------------------------------------------------------------------------------*/
+        // Inicializa del Spinner
+        Spinner spinnerFrequencyGPSConfiguration = view.findViewById(R.id.spn_frequency_options_gps);
 
+        // Obtén el array de opciones desde los recursos
+        String[] optionsFrequencyArray = getResources().getStringArray(R.array.text_frequency_options);
+
+        // Crea un ArrayAdapter usando el array de opciones y un diseño simple para el spinner
+        ArrayAdapter<String> adapterFrequency = new ArrayAdapter<>(requireActivity(), android.R.layout.simple_spinner_item, optionsFrequencyArray);
+
+        // Especifica el diseño a utilizar cuando se despliega el spinner
+        adapterFrequency.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        // Aplica el adapter al spinner
+        spinnerFrequencyGPSConfiguration.setAdapter(adapterFrequency);
+
+        // Recuperar la última selección guardada en SharedPreferences
+        int selectedFrequencyPosition = sharedPreferences.getInt(STATUS_SPN_FREQUENCY_GPS_CONFIG, 0); // 0 es el valor por defecto (primera opción)
+        // Seleccionar la opción guardada
+        spinnerFrequencyGPSConfiguration.setSelection(selectedFrequencyPosition);
+
+
+        // Guardar la selección del Spinner
+        spinnerFrequencyGPSConfiguration.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                SharedPreferences.Editor editor = sharedPreferences.edit();
+                // Guardar la posición seleccionada en SharedPreferences
+                editor.putInt(STATUS_SPN_FREQUENCY_GPS_CONFIG, position);
+                editor.apply();
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                // No se necesita implementar, pero es necesario sobrescribir el método
+            }
+        });
+
+        /*----------------------------------------------------------------------------------------*/
         return view;
     }
 
